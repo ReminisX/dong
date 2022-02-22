5f68f258e46f22cb8cfe06988f7987c2fb4ce99e
ZhangXD <1296753187@qq.com>
2022-02-22 15:51:35 +0800

    安全初步

--- src/test/java/com/zijin/dong/DongApplicationTests.java
+++ src/test/java/com/zijin/dong/DongApplicationTests.java
@@ -3,6 +3,8 @@ package com.zijin.dong;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.Pipeline;
@@ -17,6 +19,20 @@ class DongApplicationTests {
     @Test
     void contextLoads() {
         Jedis jedis = jedisPool.getResource();
+    }
+
+    @Test
+    void test(){
+        //创建解析器
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        //对密码进行加密
+        String password = encoder.encode("123");
+        System.out.println("------------"+password);
+
+        //判断原字符加密后和内容是否匹配
+        boolean result = encoder.matches("123",password);
+        System.out.println("============="+result);
     }
 
 }
