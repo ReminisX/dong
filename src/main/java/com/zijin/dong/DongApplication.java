a51f6a23cbfdbedba67172076edbf1106694bd90
ZhangXD <1296753187@qq.com>
2022-03-04 16:02:04 +0800

    添加线程池

--- src/main/java/com/zijin/dong/DongApplication.java
+++ src/main/java/com/zijin/dong/DongApplication.java
@@ -2,12 +2,14 @@ package com.zijin.dong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableScheduling
+@EnableAsync
 public class DongApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/zijin/dong/DongApplicationTests.java
+++ src/test/java/com/zijin/dong/DongApplicationTests.java
@@ -1,19 +1,27 @@
 package com.zijin.dong;
 
 import cn.hutool.core.io.FileUtil;
+import lombok.extern.slf4j.Slf4j;
 import nonapi.io.github.classgraph.utils.FileUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 @SpringBootTest
+@EnableAsync
+@Slf4j
 class DongApplicationTests {
 
     @Autowired
     private JedisPool jedisPool;
+
+    @Autowired
+    private AsyncTest asyncTest;
 
     @Test
     void contextLoads() {
@@ -24,9 +32,10 @@ class DongApplicationTests {
     private String fileSavePath;
 
     @Test
-    void test(){
-        System.out.println(fileSavePath);
-
+    void test() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            asyncTest.add();
+        }
     }
 
 }
